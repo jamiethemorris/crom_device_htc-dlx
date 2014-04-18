9be925636d9e56dde0bcd73c561472b7233dcd4e
Jamie Morris <jamiethemorris@gmail.com>
2014-04-18 14:40:22 -0700

    initial commit for c-rom
    Change-Id: I61180d4432f60ba2cdb4a27ab52aac1a905f46ae

---
+++ crom.mk
@@ -0,0 +1,34 @@
+# Release name
+PRODUCT_RELEASE_NAME := dlx
+
+# overlays
+PRODUCT_PACKAGE_OVERLAYS += device/htc/dlx/overlay
+
+# Inherit some common C-ROM stuff.
+$(call inherit-product, vendor/crom/config/common_phone.mk)
+
+# Enhanced NFC
+# $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/htc/dlx/full_dlx.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := dlx
+PRODUCT_NAME := crom_dlx
+PRODUCT_BRAND := Verizon
+PRODUCT_MODEL := HTC6435LVW
+PRODUCT_MANUFACTURER := HTC
+
+# bootanimation
+PRODUCT_COPY_FILES += \
+    vendor/crom/prebuilt/common/media/xxhdpi/BOOTANIMATION-1080x1920.zip:system/media/bootanimation.zip
+
+# Set build fingerprint / ID / Product Name etc.
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=dlx \
+    BUILD_ID=JDQ39 \
+    BUILD_FINGERPRINT=htc/verizon_wwe/dlx:4.2.2/JDQ39/277065.4:user/release-keys \
+    PRIVATE_BUILD_DESC="3.06.605.4 CL277065 release-keys"
+
+PRODUCT_GMS_CLIENTID_BASE := android-verizon
